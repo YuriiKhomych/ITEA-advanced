c532f586b9bda1b8fba59652c9b1805291358850
Valentyn <valentyn@Valentyns-MacBook-Pro.local>
2019-10-03 23:16:26 +0300

    added decorators

--- Yurii_Khomych/3_oop/hw3-valentyn/decorators.py
+++ Yurii_Khomych/3_oop/hw3-valentyn/decorators.py
@@ -1,11 +1,13 @@
 import functools
 
+
 def count_calls(func):
-    @functools.wraps(func)
+    @functools.wraps( func )
     def wrapper_count_calls(*args, **kwargs):
         wrapper_count_calls.num_calls += 1
-        print(f"Call {wrapper_count_calls.num_calls} of {func.__name__!r}")
-        return func(*args, **kwargs)
+        print( f"Call {wrapper_count_calls.num_calls} of {func.__name__!r}" )
+        return func( *args, **kwargs )
+
     wrapper_count_calls.num_calls = 0
     return wrapper_count_calls
 
@@ -13,6 +15,7 @@ def count_calls(func):
 # We need to add return statement:
 def do_twice(func):
     def wrapper(a):
-        func(a)
-        func(a)
+        func( a )
+        func( a )
+
     return wrapper
