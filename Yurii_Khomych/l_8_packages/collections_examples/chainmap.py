d7f55600233b3a109fbbfb30932eac6016fad827
yuriikhomych <yuriykhomich@gmail.com>
2019-10-24 09:16:40 +0300

    Add l_8_packages lesson examples

---
+++ Yurii_Khomych/l_8_packages/collections_examples/chainmap.py
@@ -0,0 +1,20 @@
+from collections import ChainMap
+
+baseline = {'music': 'bach', 'art': 'rembrandt'}
+adjustments = {'art': 'van gogh', 'opera': 'carmen'}
+list(ChainMap(adjustments, baseline))
+
+dict1 = {'one': 1, 'two': 2}
+dict2 = {'three': 3, 'four': 4}
+chain = ChainMap(dict1, dict2)
+
+chain
+# ChainMap({'one': 1, 'two': 2}, {'three': 3, 'four': 4})
+
+# ChainMap searches each collection in the chain
+# from left to right until it finds the key (or fails):
+chain['three']
+# 3
+chain['one']
+# 1
+chain['missing']
