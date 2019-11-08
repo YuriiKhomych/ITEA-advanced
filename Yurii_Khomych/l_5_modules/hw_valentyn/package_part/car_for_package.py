3c734badc1e082dde627d0d53c27d67c660d07c5
Valentyn <valentyn@Valentyns-MacBook-Pro.local>
2019-11-08 22:57:25 +0200

    created 5th hw

---
+++ Yurii_Khomych/l_5_modules/hw_valentyn/package_part/car_for_package.py
@@ -0,0 +1,14 @@
+class Car:
+
+    def __init__(self, model, color="Red"):
+        self.model = model
+        self.color = color
+
+
+def tripled(n):
+    return n * 3
+
+
+models = ["TeslaP", "SkodaP", "LanosP", "BMWP"]
+
+
